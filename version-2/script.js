3611981c0c7efa9d47f40b054bd9467f7f592045
Tim Cason <contact@timcasonjr.com>
2015-02-17 17:47:56 -0800

    Radial Version1 & 2

---
+++ version-2/script.js
@@ -0,0 +1,33 @@
++function() {
+    var to;
+    $(".wrap").on('mouseenter', function(){
+        var circles = $(this).children();
+        var degree = (2 * Math.PI) / circles.length; //calc delta angle
+        var transforms = [];
+    
+        // Calculate the position for each circle
+        circles.each(function (index) {
+            var x = 100 * Math.cos(-0.5 * Math.PI + degree * (-1 * index - 0.5));
+            var y = 100 * Math.sin(-0.5 * Math.PI + degree * (-1 * index - 0.5));
+            
+            transforms.push('translate(' + x + 'px,' + y + 'px)');
+        });
+        
+        // Function to moves all the circles
+        // We'll pop a circle each time and than call this function recursively
+        function moveCircles() {
+            var transform = transforms.shift();
+            circles.css('transform', transform);
+            
+            circles.splice(0,1);
+            if (circles.length) to = setTimeout(moveCircles, 400);
+        }
+        
+        moveCircles();
+    });
+    
+    $(".wrap").on('mouseleave', function(){
+        var circles = $(this).children().css('transform', '');
+        clearTimeout(to);
+    });
+}();
